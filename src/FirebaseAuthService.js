57aa8aa1f5f731683668a6d41fa4e7b0f2a56568
chechi <aesoler@proveedor.teco.com.ar>
2023-05-09 13:37:35 -0300

    first commit

---
+++ src/FirebaseAuthService.js
@@ -0,0 +1,53 @@
+import firebase from "./FirebaseConfig";
+import { getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail as sendResetMail,
+  onAuthStateChanged,
+  GoogleAuthProvider ,
+  signInWithPopup,
+} from "firebase/auth";
+
+console.log(firebase);
+const auth = getAuth(firebase);
+console.log(auth);
+
+const registerUser = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+const loginUser = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+const logoutUser = ()=> {
+  return signOut(auth);
+};
+
+const sendPasswordResetEmail = (email) => {
+  return sendResetMail(auth,email);
+};
+
+const loginWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+
+  return signInWithPopup(auth, provider);
+};
+
+const subscribeToAuthChanges = (handleAuthChange) => {
+  onAuthStateChanged(auth, user => {
+    handleAuthChange(user);
+  })
+};
+
+const FirebaseAuthService = {
+  registerUser,
+  loginUser,
+  logoutUser,
+  loginWithGoogle,
+  sendPasswordResetEmail,
+  subscribeToAuthChanges,
+};
+
+export default FirebaseAuthService;
